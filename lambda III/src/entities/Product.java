9ce0f9826f3804b82b50ee26bd896ea372a35384
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-08-03 15:41:32 -0300

    Add files via upload

---
+++ lambda III/src/entities/Product.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Product {
+
+	public Product(String string, double d) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
